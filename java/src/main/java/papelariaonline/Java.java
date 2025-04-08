97917ffafb63e1c1eb4ec79b9b862c753ef952a0
bibs <bibskies02@gmail.com>
2025-04-08 18:59:35 -0300

    08-4

---
+++ java/src/main/java/papelariaonline/Java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package papelariaonline;
+
+/**
+ *
+ * @author bibs
+ */
+public class Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
